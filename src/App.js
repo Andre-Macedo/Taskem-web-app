ac4cc6c364d6eea6a6aefd1992feae75dc676f69
Andre Macedo <andrluis@protonmail.com>
2022-08-31 18:44:07 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const list = ["aodd", "kfoaf", "faf", "dkf", "kfmf"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meu primeiro react</h1>
+      <ul>
+        {list.map(item => <li>{item}</li>)}
+      </ul>
     </div>
   );
 }
